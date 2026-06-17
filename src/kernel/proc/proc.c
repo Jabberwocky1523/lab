d2c77e7c7e71fdf1cfa65fb14d7061c2db527402
Jabberwocky1523 <439887070@qq.com>
2026-06-17 20:05:17 +0800

    modified:   src/kernel/proc/proc.c

--- src/kernel/proc/proc.c
+++ src/kernel/proc/proc.c
@@ -72,7 +72,7 @@ void proc_make_first()
     void *ustack_pa = pmem_alloc(true);
     vm_mappages(pgtbl, ustack_va, (uint64)ustack_pa, PGSIZE, PTE_R | PTE_W | PTE_U);
     proczero.ustack_npage = 1;
-    proczero.heap_top = USER_BASE + PGSIZE;  // code+data 之后
+    proczero.heap_top = USER_BASE + PGSIZE; // code+data 之后
 
     // 5. 为 code+data 分配物理页, 拷贝 initcode, 完成映射
     void *code_pa = pmem_alloc(true);
@@ -83,16 +83,16 @@ void proc_make_first()
     tf->user_to_kern_satp = MAKE_SATP(kernel_pgtbl);
     tf->user_to_kern_sp = KSTACK(0) + PGSIZE;
     tf->user_to_kern_trapvector = (uint64)trap_user_handler;
-    tf->user_to_kern_epc = USER_BASE;            // 用户程序入口 (initcode)
+    tf->user_to_kern_epc = USER_BASE; // 用户程序入口 (initcode)
     tf->user_to_kern_hartid = 0;
-    tf->sp = ustack_va + PGSIZE;                 // 用户栈顶 (栈向下增长)
+    tf->sp = ustack_va + PGSIZE; // 用户栈顶 (栈向下增长)
 
     // 7. 设置内核上下文: 下次 swtch 回来时从这里开始执行
     proczero.tf = tf;
     proczero.pgtbl = pgtbl;
     proczero.kstack = KSTACK(0);
-    proczero.ctx.ra = (uint64)trap_user_return;  // swtch 后跳转到 trap_user_return
-    proczero.ctx.sp = KSTACK(0) + PGSIZE;        // 内核栈顶
+    proczero.ctx.ra = (uint64)trap_user_return; // swtch 后跳转到 trap_user_return
+    proczero.ctx.sp = KSTACK(0) + PGSIZE;       // 内核栈顶
 
     // 8. 绑定到当前 CPU 并通过 swtch 完成上下文切换
     mycpu()->proc = &proczero;
